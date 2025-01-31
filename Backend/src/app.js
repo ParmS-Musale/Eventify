8ff9109beec1f000964c7c8ddcc02c0047d9576b
ParmS-Musale <musaleparm9541@gmail.com>
2025-01-31 21:14:39 +0530

    Update CORS configuration to allow requests from localhost

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,9 +11,9 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTED_URL, // Allow requests from your frontend
+    origin: "http:localhost:5173", // Allow requests from your frontend
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"], // Specify allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    // allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
     credentials: true,
   })
 );
